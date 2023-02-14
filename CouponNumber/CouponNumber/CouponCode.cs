aedc23c6910151a80caeafc294cd39ac99dee454
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-02-14 21:08:11 +0530

    [Sangmeshwar] Add: Coupon Number Problem

---
+++ CouponNumber/CouponNumber/CouponCode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CouponNumber
+{
+    public class CouponCode
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter length of coupons");
+            int number = int.Parse(Console.ReadLine());
+            string coupons = string.Empty;
+            while (number > 0)
+            {
+                int random = new Random().Next(1, 123);
+                coupons += (char)random;
+                number--;
+            }
+            Console.WriteLine("Your coupon number is " + coupons);
+            Console.ReadLine();
+        }
+    }
+}
